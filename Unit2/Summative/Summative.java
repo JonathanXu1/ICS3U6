c3b3954c7c7081c6e597288ee2e4004d8d06badb
Jonathan Xu <jonathanlikestoemail@gmail.com>
2018-04-10 14:06:13 -0400

    Some stuff

--- Unit2/Summative/Summative.java
+++ Unit2/Summative/Summative.java
@@ -11,11 +11,9 @@ import java.io.File;
 import java.io.PrintWriter;
 
 public class Summative {
-  public static ArrayList<String> path = new ArrayList<String>();
   public static void main(String[] args) throws Exception{
     Scanner input1 = new Scanner(System.in);
     String response;
-    
     
     // Welcome
     System.out.println("Welcome to the monster hunter mini world!");
@@ -35,19 +33,31 @@ public class Summative {
       System.out.println();
     }
     
+    //Finds paths
     System.out.println("All possible paths:");
-    findpath(1, 1, 1, world, "");
-    for(String s:path){
-      System.out.println(s);
+    String paths = findpath(1, 1, 1, world, "");
+    for (int i = 0; i < paths.length(); i++){
+      if (paths.substring(i, i+1).equals("*")){
+        System.out.println();
+      }
+      else{
+        System.out.print(paths.substring(i,i+1));
+      }
     }
+    
+    //Outputs solution
+    /*
+    String[][] solution = 
+    */
+    File output = new File("solution.txt");
+    PrintWriter output = new PrintWriter(output);
+    
     
     input1.close();   
     input2.close();
   }
   
-  /**
-   * keypoints
-   */
+  //Convert map to ints
   public static int[][] loadMap(Scanner in) throws Exception{
     String symbol;
     String line = in.nextLine();
@@ -57,7 +67,6 @@ public class Summative {
     int[][] map = new int[len][len];
     
     for (int i = 0; i < len; i++){
-      count ++;
       for (int j = 0; j < len; j++){
         symbol = line.substring(j, j+1);
         if ("+-|".contains(symbol)){
@@ -73,6 +82,7 @@ public class Summative {
           map[i][j] = 4;
         }
       }
+      count ++;
       if (count <= len){
         line = in.nextLine();
       } 
@@ -83,32 +93,54 @@ public class Summative {
   
   
   /**
-   * keypoints
+   * Finds all paths
    */
-  public static void findpath(int x, int y, int step, int[][] map, String line) throws Exception {
+  public static String findpath(int x, int y, int step, int[][] map, String line) throws Exception {
     int prevNumber = map[y][x];
     map[y][x] = 1; //Traveled places
     step ++;
+    String output = "";
     
     if (x == map.length-1 && y == map.length-2){ //If met destinatinon
-      path.add(line);
+      output = line + "*";
     }
-    else if (map[y-1][x] == 1 && map[y][x+1] == 1 && map[y+1][x] == 1 && map[y][x-1] == 1){
+    else if (map[y-1][x] == 1 && map[y][x+1] == 1 && map[y+1][x] == 1 && map[y][x-1] == 1){ //If deadend
+      output = "";
     }
     else {
       if(map[y-1][x] != 1){
-        findpath(x, y-1, step, map, line + "U");
+        output += findpath(x, y-1, step, map, line + "U");
       }
       if(map[y][x+1] != 1){
-        findpath(x+1, y, step, map, line + "R");
+        output += findpath(x+1, y, step, map, line + "R");
       }
       if(map[y+1][x] != 1){
-        findpath(x, y+1, step, map, line + "D");
+        output += findpath(x, y+1, step, map, line + "D");
       }
       if(map[y][x-1] != 1){
-        findpath(x-1, y, step, map, line + "L");
+        output += findpath(x-1, y, step, map, line + "L");
       } 
     }
     map[y][x]=prevNumber;
+    return output;
+  }
+  
+  /*
+   *Place traps
+  */
+  public static String[][] placeTrap(int[][] map, String dir){
+    int count = 0;
+    for(int i = 0; i<dir.length(); i++){ //Seperates the single line of paths into an array
+      if(dir.substring(i, i+1).equals("*")){
+        count++;
+      }
+    }
+    String[] paths = new String[count];
+    for (int i = 0; i < count; i ++){
+      paths[i] = dir.substring(0, dir.indexOf("*"));
+      dir = dir.substring(dir.indexOf("*") + 1);
+    }
+    
+    
   }
 }
