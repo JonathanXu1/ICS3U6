aaceba757394df6d3c501430f7d4fdb5cf770ba3
Jonathan Xu <jonathanlikestoemail@gmail.com>
2018-05-05 23:59:27 -0400

    default wolf settings

--- Unit3/Summative/Wolf.java
+++ Unit3/Summative/Wolf.java
@@ -53,7 +53,7 @@ class Wolf extends Animal{
           nextx = rand.nextInt(3) -1;
           nexty = rand.nextInt(3) -1;
         } while(nexty+y < 0 || nexty+y >= map.length || nextx+x < 0 || nextx+x >= map[0].length);
-      } while( !(map[nexty+y][nextx+x] instanceof Animal) && (nextx != 0 || nexty != 0) ); //Comparable interface somehow
+      } while( map[nexty+y][nextx+x] instanceof Animal && (nextx != 0 || nexty != 0) ); //Comparable interface somehow
       option = (nexty+1)*3 + nextx + 2;
     }
 
